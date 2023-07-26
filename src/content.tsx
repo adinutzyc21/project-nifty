e8e4b89a0c85361077cc2322e2fbf078101f8b31
Adina Stoica <adina.stoica@gmail.com>
2023-07-26 00:47:58 -0400

    WIP

--- src/content.tsx
+++ src/content.tsx
@@ -16,3 +16,20 @@ const app = document.createElement('div');
 app.id = "my-extension-root";
 document.body.appendChild(app);
 ReactDOM.render(<Main />, app);
+
+app.style.display = "none";
+chrome.runtime.onMessage.addListener(
+    function (request: any, sender: any, sendResponse: any) {
+        if (request.message === "toggleExtension") {
+            toggle();
+        }
+    }
+);
+
+function toggle() {
+    if (app.style.display === "none") {
+        app.style.display = "block";
+    } else {
+        app.style.display = "none";
+    }
+}
